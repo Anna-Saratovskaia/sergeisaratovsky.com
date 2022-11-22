d9c8b28e430da709df95288db04b3ce81a935515
Anna-Saratovskaia <94197450+Anna-Saratovskaia@users.noreply.github.com>
2022-11-22 12:54:42 -0800

    Solve contact form receiving problem

--- mail.php
+++ mail.php
@@ -5,10 +5,11 @@ $student_name = $_POST['student_name'];
 $parent_name = $_POST['parent_name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent=" Parent: $parent_name \nStudent: $student_name \nEmail: $email \nMessage: $message";
-$recipient = "sergei_saratovsky@hotmail.com";
+$formcontent = "Parent: $parent_name \nStudent: $student_name \nEmail: $email \nMessage: $message";
+$recipient = "info@sergeisaratovsky.com";
 $subject = "Website form message";
-$mailheader = "From: $email \r\n";
+$mailheader = "From: info@sergeisaratosvsky.com \r\n";
+$headers .= "Reply-To: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='./' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
